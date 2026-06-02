e7475763731b40588cf1825c497a4552f9dbdc66
Rukaiya Nishat Rima <rimarukaiya8@gmail.com>
2026-06-02 11:20:12 +0600

    backup -code of rukaiya

--- Homepage.java
+++ Homepage.java
@@ -1,66 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
-package homepage;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-/**
- *
- * @author USER
- */
-public class Homepage extends Application {
-    
+public class HomePage extends Application {
+
     @Override
     public void start(Stage stage) {
-
-        // Logo
-        Label logo = new Label("🍽");
-        logo.setFont(new Font(40));
-
-        // Restaurant Name
-        Label name = new Label("ROYAL TASTE");
-        name.setFont(new Font(28));
-
-        // Tagline
-        Label tagline = new Label("Fresh Food, Happy Mood ❤️");
-        tagline.setFont(new Font(16));
-
-        // History Section
-        Label history = new Label(
-                "Our restaurant started in 2015.\n" +
-                "We serve fresh and hygienic food."
-        );
-
-        history.setFont(new Font(14));
-
-        // Layout
-        VBox root = new VBox(15);
-        root.getChildren().addAll(
-                logo,
-                name,
-                tagline,
-                history
-        );
-
+        Label title = new Label("Restaurant Menu Interface");
+        Button menuBtn = new Button("View Menu");
+        Button orderBtn = new Button("Order Now");
+        Button exitBtn = new Button("Exit");
+        VBox root = new VBox(20);
         root.setAlignment(Pos.CENTER);
 
-        // Scene
-        Scene scene = new Scene(root, 700, 450);
+        root.getChildren().addAll(title, menuBtn, orderBtn, exitBtn);
+        Scene scene = new Scene(root, 500, 400);
 
-        // Stage
-        stage.setTitle("Restaurant App");
+      
+        stage.setTitle("Home Page");
         stage.setScene(scene);
         stage.show();
     }
